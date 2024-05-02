f40761848d58d77082a38815d3d1b7582c36a0a5
Daria Liferova <liferova.daria@gmail.com>
2024-05-02 11:31:41 +0200

    Install Tailwind Inter Plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import './App.css'
+import "./App.css"
+import LimitationCard from "./components/ui/limitationCard.tsx"
 
 function App() {
   return (
-    <>
+    <div className="text-black font-inter">
       <h1 className="text-3xl font-bold underline">
-      Hello world with Tailwind!
-    </h1>
-    </>
+        Hello world with Tailwind!
+      </h1>
+      <LimitationCard />
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#345FF6",
+        gunmetal: "#253347",
+        "dark-electric-blue": "#5E6E85",
+        borders: "#D8E2E7",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss-font-inter")({
+      baseFontSize: 16,
+    }),
+  ],
 }
-
